47c8e09913ba9173acfbbda349a1bd6db33ccbf6
lft-mx <lftcalculator@gmail.com>
2026-04-07 11:49:07 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,14 @@
 function toggleModo() {
   document.body.classList.toggle("dark");
 }
+let idioma = "es";
+
+function toggleIdioma() {
+  idioma = idioma === "es" ? "en" : "es";
+
+  if (idioma === "en") {
+    document.getElementById("titulo").innerText = "Work Calculator Mexico 🇲🇽";
+  } else {
+    document.getElementById("titulo").innerText = "Calculadora Laboral México 🇲🇽";
+  }
+}
